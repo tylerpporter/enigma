f8cf3ea1589adb181cf22f93c59ef306b6cbe9ae
Tyler Porter <tylerpporter@gmail.com>
2020-02-27 20:35:35 -0700

    Create Cipher class

---
+++ lib/cipher.rb
@@ -0,0 +1,9 @@
+
+class Cipher
+  attr_reader :alphabet
+
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+  end
+
+end
---
+++ test/cipher_test.rb
@@ -0,0 +1,47 @@
+require_relative 'test_helper.rb'
+require './lib/cipher'
+
+class CipherTest < Minitest::Test
+
+  def setup
+    @cipher = Cipher.new
+  end
+
+  def test_it_exists
+    assert_instance_of Cipher, @cipher
+  end
+
+  def test_it_has_attributs
+    expected = [
+     "a",
+     "b",
+     "c",
+     "d",
+     "e",
+     "f",
+     "g",
+     "h",
+     "i",
+     "j",
+     "k",
+     "l",
+     "m",
+     "n",
+     "o",
+     "p",
+     "q",
+     "r",
+     "s",
+     "t",
+     "u",
+     "v",
+     "w",
+     "x",
+     "y",
+     "z",
+     " "
+    ]
+    assert_equal expected, @cipher.alphabet
+  end
+
+end
