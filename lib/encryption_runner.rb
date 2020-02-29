27262a3c72f80a0303792f5dfc1ac07337252206
Tyler Porter <tylerpporter@gmail.com>
2020-02-28 21:26:37 -0700

    Create Runner class methods and update runner files

--- lib/encryption_runner.rb
+++ lib/encryption_runner.rb
@@ -3,18 +3,33 @@ require_relative './enigma.rb'
 class Runner
 
   def self.encrypt
-    file_a = File.open(ARGV[0], "r")
-    message = file_a.read
+    file = File.open(ARGV[0], "r")
+    message = file.read
     enigma = Enigma.new
     @@encryption = enigma.encrypt(message.strip)
-    file_a.close
+    file.close
   end
 
   def self.write_encryption
-    file_b = File.open(ARGV[1], "w")
-    file_b.write(@@encryption[:encryption])
-    file_b.close
+    file = File.open(ARGV[1], "w")
+    file.write(@@encryption[:encryption])
+    file.close
     puts "Created '#{ARGV[1]}' with the key #{@@encryption[:key]} and date #{@@encryption[:date]}"
+  end
+
+  def self.decrypt
+    file = File.open(ARGV[0], "r")
+    message = file.read
+    enigma = Enigma.new
+    @@decryption = enigma.decrypt(message, ARGV[2], ARGV[3])
+    file.close
+  end
+
+  def self.write_decryption
+    file = File.open(ARGV[1], "w")
+    file.write(@@decryption[:decryption])
+    file.close
+    puts "Created '#{ARGV[1]}' with the key #{@@decryption[:key]} and date #{@@decryption[:date]}"
   end
 
 end
