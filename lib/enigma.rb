1daf2bfb8fc435fe8b5fda1bd6b3bdadc69657d1
Tyler Porter <tylerpporter@gmail.com>
2020-02-28 20:18:44 -0700

    Create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,6 +8,14 @@ class Enigma < Cipher
     super
   end
 
-  
+  def encrypt(message, key = random_num, date = today)
+    encrypted = cipher(message, key, date)
+    clear
+    {encryption: encrypted, key: key, date: date}
+  end
+
+  def decrypt(message, key, date = today)
+    cipher(message, key, date, :decrypt)
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -45,4 +45,50 @@ class EnigmaTest < Minitest::Test
     assert_equal [], @enigma.new_message
   end
 
+  def test_it_generates_the_date_as_string
+    Date.stubs(:today).returns(Date.new(2020,2,27))
+    assert_equal "270220", @enigma.today
+  end
+
+  def test_it_generates_random_0_padded_5_digit_number_as_string
+    @enigma.stubs(:rand).returns(2715)
+    assert_equal "02715", @enigma.random_num
+  end
+
+  def test_it_can_encrypt_a_message_with_given_key_and_date
+    expected1 = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected1, @enigma.encrypt("hello world", "02715", "040895")
+    expected2 = {
+      encryption: "vjqtbeaweqihssi",
+      key: "08304",
+      date: "291018"
+      }
+      assert_equal expected2, @enigma.encrypt("hello world end", "08304", "291018")
+  end
+
+  def test_it_can_encrypt_a_message_with_given_key_and_no_date
+    Date.stubs(:today).returns(Date.new(1995,8,4))
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
+  end
+
+  def test_it_can_encrypt_a_message_with_no_date_and_no_key
+    Date.stubs(:today).returns(Date.new(1995,8,4))
+    @enigma.stubs(:rand).returns(2715)
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, @enigma.encrypt("hello world")
+  end
+
 end
